e42eca796ea2d161a8db3440c216d3baaed5124a
Diana Vu <aznalive@hotmail.com>
2022-11-17 21:17:17 -0500

    added temp resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,7 +1,14 @@
+import React from 'react';
+import resumee from '../assets/resume/resume.png'
+
 export const Resume = () => {
   return (
-    <section className='Resume'>
-      testing
-    </section>
+                <div className="resume"> 
+                <p>(This resume is just a filler while I work on my current one!)</p>
+                    <h1>View or download</h1>
+                    <a href={resumee} className="logoColor">
+                        <h3 className='profText'>My Resume</h3>
+                    </a>
+                </div>
   )
 }
